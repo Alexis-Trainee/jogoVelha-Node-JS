4be17558839eb37133375cbf1d52909dbd23f237
Alexis <alexysmcf@gmail.com>
2020-11-16 19:16:02 -0300

    indentation correction, renaming variables ...

--- jogo-da-velha.mjs
+++ jogo-da-velha.mjs
@@ -1,6 +1,6 @@
 //------------------------------------JOGO DA VELHA-------------------------------------//
 //--------------------------------------------------------------------------------------//
-import { checkWinners } from './gameVelha2.mjs';
+import { checkWinners } from './complemento-jogo-da-velha.mjs';
 import readlineSync from 'readline-sync';
 
 console.log("**********JOGO DA VELHA**********\n");
@@ -13,37 +13,39 @@ let board = [
 console.log('>>>A primeira jogada sempre se iniciará por  X <<<\n\n');
 console.table(board);
 
-let runGame = true;
-let player = 'x';
-let cont = 0;
+const dataInput=(data)=>{
+    let checkData= true;
+    let result;
 
-while(runGame){
-let dataInput=(data)=>{
-let test= true;
-let result;
-    
-    while(test) {                                             
-   const input = readlineSync.question('  Digite um valor para a  ' + data + ' que esteja entre 0 e 2:\n');
- if (input>=0 && input<=2) {
-    result=input;
-    test=false;
+    while(checkData) {                                             
+        const input = readlineSync.question('  Digite um valor para a  ' + data + ' que esteja entre 0 e 2:\n');
+        if (input>=0 && input<=2) {
+            result=input;
+            checkData=false;
 
         }else {
             console.log ('>>>' ,input, 'NÂO É UM VALOR VALIDO PARA A LINHA!!!<<<\n');     
         }
     }
     return result;
-}   
-const line = dataInput('LINHA');
-const column = dataInput('COLUNA');
+}
 
-   if(board[line][column]==='-'){
-        board[line][column] = player;
+let runGame = true;
+let player = 'x';
+let cont = 0;
+
+while(runGame) {
+    const line = dataInput('LINHA');
+    const column = dataInput('COLUNA');
+
+    if(board[line][column]==='-'){
+        board[line][column] = player; 
         cont = cont+1;
         console.table(board);
 
     }else {
         console.log ('ESTÁ COORDENADA JÁ FOI PREENCHIDA. POR FAVOR, TENTE OUTRA COORDENADA!');      
+        continue;    
     }
 
     if (cont >= 5) {
@@ -56,4 +58,5 @@ const column = dataInput('COLUNA');
     }else {
         player = 'x';
     } 
+
 }//runGame
---
+++ versões_do_jogo./texte5.mjs
@@ -0,0 +1,62 @@
+
+//------------------------------------JOGO DA VELHA-------------------------------------//
+//--------------------------------------------------------------------------------------//
+import { checkWinners } from './complemento-jogo-da-velha.mjs';
+import readlineSync from 'readline-sync';
+
+console.log("**********JOGO DA VELHA**********\n");
+let board = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+];
+
+console.log('>>>A primeira jogada sempre se iniciará por  X <<<\n\n');
+console.table(board);
+
+let runGame = true;
+let player = 'x';
+let cont = 0;
+
+while(runGame){
+let dataInput=(data)=>{
+let test1= true;
+let resultado;
+    
+    while(test1) {                                             
+   const input = readlineSync.question('  Digite um valor para a  ' + data + ' que esteja entre 0 e 2:\n');
+ if (input>=0 && input<=2) {
+    resultado=input;
+    test1 =false;
+
+        }else {
+            console.log ('>>>' ,input, 'NÂO É UM VALOR VALIDO PARA A LINHA!!!<<<\n');     
+        }
+
+    }
+    return resultado;
+}    
+const line = dataInput('LINHA');
+const column = dataInput('COLUNA');
+
+   if(board[line][column]==='-'){
+        board[line][column] = player;
+        cont = cont+1;
+        console.table(board);
+
+    }else {
+        console.log ('ESTÁ COORDENADA JÁ FOI PREENCHIDA. POR FAVOR, TENTE OUTRA COORDENADA!');      
+    }
+
+    if (cont >= 5) {
+        checkWinners(board, cont);
+    }
+
+    if(player === 'x') {
+        player = '0';
+
+    }else {
+        player = 'x';
+    } 
+
+}//runGame
